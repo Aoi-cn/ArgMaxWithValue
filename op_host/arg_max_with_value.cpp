b9186b7735f9e4e22e1e27a8514be65920860aaf
Aoi-cn <1930001809@qq.com>
2024-12-07 13:45:23 +0800

    可编译版本

--- op_host/arg_max_with_value.cpp
+++ op_host/arg_max_with_value.cpp
@@ -60,7 +60,7 @@ public:
 
         this->AICore()
             .SetTiling(optiling::TilingFunc);
-        this->AICore().AddConfig("ascend910");
+        this->AICore().AddConfig("ascend910b");
 
     }
 };
--- test.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-
-input_x = np.random.uniform(0, 127, [13, 171, 351]).astype(np.uint8)
-dimension = 0
-keep_dims = False
